691b9d5715523f34d9f95eb8f8eb9161cd7b1321
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-08-15 17:00:05 -0300

    Update
    15.08 - 16:59
    
    Feito botão de cálculo de frente no detalhe do produto

--- src/app/componentes/telas/detalhe-produto/detalhe-produto.component.ts
+++ src/app/componentes/telas/detalhe-produto/detalhe-produto.component.ts
@@ -14,6 +14,7 @@ export class DetalheProdutoComponent implements OnInit {
   produto: Produtos | undefined;
   nomeProdutoFormatado: string | null = null;
   produtoDaCategoria!: Produtos;
+  cep!: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -80,38 +81,3 @@ export class DetalheProdutoComponent implements OnInit {
 }
 
 
-//================================================================================================================================//
-    //RECEBER OS PARAMETROS DOS PRODUTOS EM DESTAQUE
-    // this.route.params.subscribe((params) => {
-    // this.nomeProduto = params['nome'];
-
-    // if (this.nomeProduto) {
-    //   // Substitua os hífens por espaços para obter o nome original do produto
-    //   const nomeOriginal = this.nomeProduto.replace(/-/g, ' ');
-
-    //   // Obter produto dos produtos em destaque
-    //   this.produto = this.produtoService.obterProdutoPorNomeDestaque(nomeOriginal);
-
-    // //================================================================================================================================//
-    // //RECEBER OS PARAMETROS DOS PRODUTOS MAIS VENDIDOS
-    // // Obter produto dos produtos mais vendidos
-    //   const produtoMaisVendido = this.produtoService.obterProdutoPorNomeMaisVendidos(nomeOriginal);
-
-    // // Verificar se o produto foi encontrado nos produtos mais vendidos
-    //   if (produtoMaisVendido) {
-    //     // Se foi encontrado, atualize o valor do produto
-    //     this.produto = produtoMaisVendido;
-    //   }
-
-    // //================================================================================================================================//
-    // //RECEBER OS PARAMETROS DOS PRODUTOS MAIS VENDIDOS
-    // // Obter produto dos produtos mais vendidos
-    // const produtoEmPromocao = this.produtoService.obterProdutoPorNomeEmPromocao(nomeOriginal);
-
-    // // Verificar se o produto foi encontrado nos produtos mais vendidos
-    //   if (produtoEmPromocao) {
-    //     // Se foi encontrado, atualize o valor do produto
-    //     this.produto = produtoEmPromocao;
-    //   }
-    // }
-    // });
