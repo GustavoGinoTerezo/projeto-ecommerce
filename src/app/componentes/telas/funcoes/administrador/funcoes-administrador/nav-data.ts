456af9ded2551ab432dd7bb2ec65da941b37cb4b
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-09-12 17:38:52 -0300

    Update

--- src/app/componentes/telas/funcoes/administrador/funcoes-administrador/nav-data.ts
+++ src/app/componentes/telas/funcoes/administrador/funcoes-administrador/nav-data.ts
@@ -7,11 +7,6 @@ export const navbarData: INavbarData[] = [
     label: 'Gerenciamento de Clientes',
   },
   {
-    routerLink: 'relatorio-de-vendas-e-controle-de-pedidos',
-    icon: 'fa-solid fa-magnifying-glass-chart',
-    label: 'Relatório de Vendas e Controle de Pedidos',
-  },
-  {
     routerLink: 'gerenciamento-de-categorias-e-produtos',
     icon: 'fa-brands fa-shopify',
     label: 'Gerenciamento de Categorias e Produtos',
@@ -22,14 +17,19 @@ export const navbarData: INavbarData[] = [
     label: 'Gerenciamento de Estoque',
   },
   {
-    routerLink: 'entrada-de-notas-fiscais',
-    icon: 'fa-solid fa-file-invoice-dollar',
-    label: 'Entrada de Notas Fiscais',
-  },
-  {
     routerLink: 'gerenciamento-de-comentarios',
     icon: 'fa-solid fa-comment-dots',
     label: 'Gerenciamento de Comentários',
+  },
+  {
+    routerLink: 'relatorio-de-vendas-e-controle-de-pedidos',
+    icon: 'fa-solid fa-magnifying-glass-chart',
+    label: 'Relatório de Vendas e Controle de Pedidos',
+  },
+  {
+    routerLink: 'entrada-de-notas-fiscais',
+    icon: 'fa-solid fa-file-invoice-dollar',
+    label: 'Entrada de Notas Fiscais',
   },
   {
     routerLink: 'personalizacao',
