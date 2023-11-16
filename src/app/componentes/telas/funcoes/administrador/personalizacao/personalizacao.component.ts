5aa689cdccfdfa195296f52f9d0b72e3dd318a65
Gustavo Gino Terezo <gustavogino@outlook.com.br>
2023-11-16 14:51:12 -0300

    Update
    16.11 - 14:50
    
    Criação da estrutura para as cores

--- src/app/componentes/telas/funcoes/administrador/personalizacao/personalizacao.component.ts
+++ src/app/componentes/telas/funcoes/administrador/personalizacao/personalizacao.component.ts
@@ -11,6 +11,14 @@ export class PersonalizacaoComponent {
   cor1: string = ''; 
   cor2: string = '';
   cor3: string = '';
+  cor4: string = ''; 
+  cor5: string = '';
+  cor6: string = '';
+  cor7: string = ''; 
+  cor8: string = '';
+  cor9: string = '';
+  cor10: string = '';
+  cor11: string = '';
 
   constructor(private colorService: ServiceColorPickerService ) {}
 
@@ -28,7 +36,47 @@ export class PersonalizacaoComponent {
 
     this.colorService.ton3$.subscribe(novaCor => {
       const estilo = document.documentElement.style;
+      estilo.setProperty('--scrollbar', novaCor);
+    });
+
+    this.colorService.ton4$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
       estilo.setProperty('--textos-e-icones-botoes', novaCor);
+    });
+
+    this.colorService.ton5$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--background-botoes', novaCor);
+    });
+
+    this.colorService.ton6$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--hover-background-botoes', novaCor);
+    });
+
+    this.colorService.ton7$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--container-externo-cards-produtos', novaCor);
+    });
+
+    this.colorService.ton8$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--container-interno-cards-produtos', novaCor);
+    });
+
+    this.colorService.ton9$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--borda-dos-containers', novaCor);
+    });
+
+    this.colorService.ton10$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--background-das-divs-da-categoria', novaCor);
+    });
+
+    this.colorService.ton11$.subscribe(novaCor => {
+      const estilo = document.documentElement.style;
+      estilo.setProperty('--cor-valor-total', novaCor);
     });
   }
 
@@ -53,6 +101,70 @@ export class PersonalizacaoComponent {
     if (novaCor && novaCor.value) {
       const corSelecionada = novaCor.value;
       this.colorService.atualizarTon3(corSelecionada);
+
+    }
+  }
+
+  atualizarCor4(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon4(corSelecionada);
+
+    }
+  }
+
+  atualizarCor5(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon5(corSelecionada);
+
+    }
+  }
+
+  atualizarCor6(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon6(corSelecionada);
+
+    }
+  }
+
+  atualizarCor7(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon7(corSelecionada);
+
+    }
+  }
+
+  atualizarCor8(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon8(corSelecionada);
+
+    }
+  }
+
+  atualizarCor9(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon9(corSelecionada);
+
+    }
+  }
+
+  atualizarCor10(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon10(corSelecionada);
+
+    }
+  }
+
+  atualizarCor11(novaCor: any) {
+    if (novaCor && novaCor.value) {
+      const corSelecionada = novaCor.value;
+      this.colorService.atualizarTon11(corSelecionada);
 
     }
   }
--- src/app/services/serviceColorPicker/service-color-picker.service.ts
+++ src/app/services/serviceColorPicker/service-color-picker.service.ts
@@ -9,14 +9,38 @@ export class ServiceColorPickerService {
   constructor() { }
 
   //Declarando os tons como observaveis
-  private ton1 = new BehaviorSubject<string>('rgba(131, 125, 125, 0.829)');
+  private ton1 = new BehaviorSubject<string>('');
   ton1$ = this.ton1.asObservable();
 
-  private ton2= new BehaviorSubject<string>('#6a6a6efd');
+  private ton2 = new BehaviorSubject<string>('');
   ton2$ = this.ton2.asObservable();
 
-  private ton3 = new BehaviorSubject<string>('rgb(41,44,47)');
+  private ton3 = new BehaviorSubject<string>('');
   ton3$ = this.ton3.asObservable();
+
+  private ton4 = new BehaviorSubject<string>('');
+  ton4$ = this.ton4.asObservable();
+
+  private ton5 = new BehaviorSubject<string>('');
+  ton5$ = this.ton5.asObservable();
+
+  private ton6 = new BehaviorSubject<string>('');
+  ton6$ = this.ton6.asObservable();
+
+  private ton7 = new BehaviorSubject<string>('');
+  ton7$ = this.ton7.asObservable();
+
+  private ton8 = new BehaviorSubject<string>('');
+  ton8$ = this.ton8.asObservable();
+
+  private ton9 = new BehaviorSubject<string>('');
+  ton9$ = this.ton9.asObservable();
+
+  private ton10 = new BehaviorSubject<string>('');
+  ton10$ = this.ton10.asObservable();
+
+  private ton11 = new BehaviorSubject<string>('');
+  ton11$ = this.ton11.asObservable();
 
   atualizarTon1(novaCor: string) {
     this.ton1.next(novaCor);
@@ -28,5 +52,37 @@ export class ServiceColorPickerService {
 
   atualizarTon3(novaCor: string) {
     this.ton3.next(novaCor);
+  }
+
+  atualizarTon4(novaCor: string) {
+    this.ton4.next(novaCor);
+  }
+
+  atualizarTon5(novaCor: string) {
+    this.ton5.next(novaCor);
+  }
+
+  atualizarTon6(novaCor: string) {
+    this.ton6.next(novaCor);
+  }
+
+  atualizarTon7(novaCor: string) {
+    this.ton7.next(novaCor);
+  }
+
+  atualizarTon8(novaCor: string) {
+    this.ton8.next(novaCor);
+  }
+
+  atualizarTon9(novaCor: string) {
+    this.ton9.next(novaCor);
+  }
+
+  atualizarTon10(novaCor: string) {
+    this.ton10.next(novaCor);
+  }
+
+  atualizarTon11(novaCor: string) {
+    this.ton11.next(novaCor);
   }
 }
